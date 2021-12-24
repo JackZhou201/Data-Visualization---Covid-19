ab80336c5fe2559a0889873f374fe6080947db7c
JackZhou201 <96538733+JackZhou201@users.noreply.github.com>
2021-12-24 12:21:49 -0500

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from datetime import datetime
+
+
+dataset = requests.get("https://data.cdc.gov/resource/9mfq-cb36.json")
+print(dataset.status_code)
+dataset = dataset.json()
+
+def extract_time(json):
+    try:
+        dt = datetime.fromisoformat(json['created_at'])
+        return dt
+    except KeyError:
+        return 0
+
+dataset.sort(key=extract_time, reverse=True)
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+with open('datas.txt', 'w') as outfile:
+    json.dump(dataset, outfile)
