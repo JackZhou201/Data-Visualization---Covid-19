851b82d336a0e3ba9a8054bcf0e2119d74e6c077
JackZhou201 <96538733+JackZhou201@users.noreply.github.com>
2021-12-24 12:25:48 -0500

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-
+# pull request from cdc website 
 dataset = requests.get("https://data.cdc.gov/resource/9mfq-cb36.json")
 print(dataset.status_code)
 dataset = dataset.json()
@@ -14,13 +14,15 @@ def extract_time(json):
     except KeyError:
         return 0
 
+# sort data entries by creation date
 dataset.sort(key=extract_time, reverse=True)
 
+# nice printing 
 def jprint(obj):
-    # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
+# write data to file
 with open('datas.txt', 'w') as outfile:
     text = json.dumps(dataset, sort_keys=True, indent=4)
     outfile.write(text)
